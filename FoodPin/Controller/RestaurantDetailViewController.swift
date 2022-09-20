6288fffd7443ebcf285148be849eedfeb0ef661b
AdamIwasz <adam.iwaszkiewiczai@gmail.com>
2022-09-20 22:41:41 +0200

    Added restaurantDetailViewController and restaurantDetailView in storyboard filled with UIImageView

---
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by Adam Iwaszkiewicz UCTI on 20/09/2022.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController {
+
+    @IBOutlet var restaurantImageView: UIImageView!
+
+    var restaurantImageName = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
